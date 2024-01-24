8e57d386c71bbdd368c535ab1a71b99e84458b69
Medonca09 <lucasmm0908@gmail.com>
2024-01-24 09:27:45 -0300

    Aula2: tipo string - introdução aos tipos de dados

---
+++ aula2.py
@@ -0,0 +1,15 @@
+print(1234)
+
+# Aspas simples
+print('Lucas Mendonça')
+print(1, 'Lucas "Mendonça"')
+
+# Aspas duplas
+print("Lucas Mendonça")
+print(2, "Lucas 'Mendonça'")
+
+# Escape
+print("Lucas \"Mendonça\"")
+
+# r
+print(r"Lucas \"Mendonça\"")
