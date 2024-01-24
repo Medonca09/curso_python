d159ada3130295772e8befd13e07e83fe965bdd7
Medonca09 <lucasmm0908@gmail.com>
2024-01-24 10:07:13 -0300

    Exerício: exercício com variáveis e tipo de dados

---
+++ exercicios/exercicio1(aula8).py
@@ -0,0 +1,15 @@
+# Exercício da aula 8, onde devemos trazer os dados através das variáveis
+
+nome = 'Lucas'
+sobrenome = 'Mendonça'
+idade = 20
+ano_nascimento = 2024 - idade
+maior_idade = idade >= 18
+altura_metros = 1.83
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_idade)
+print('Altura em metros:', altura_metros)
